3367cae79a8ae87b7b12a38de49bc3e7b3e4188c
Сергій Когут <sergio.kogut@gmail.com>
2018-05-15 10:59:10 +0300

    start

---
+++ Money/Money.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MoneySpace
+{
+    partial class Program
+    {
+        class Money
+        {
+            public int Grivna { get; private set; }
+            public int Cent { get; private set; }
+
+            public Money()
+            {
+                this.Grivna = 0;
+                this.Cent = 0;
+            }
+
+            public Money(double amount)
+            {
+                this.Grivna = (int)(Math.Floor(amount));
+                this.Cent = (int)((amount - Math.Floor(amount))*100);
+            }
+
+
+            public Money(int grn,int kop)
+            {
+                this.Grivna = grn;
+                this.Cent = kop;
+            }
+
+            public override string ToString()
+            {
+                return $"{ this.Grivna} грн {this.Cent} копеек";
+            }
+
+            private int AsCent(double amount)
+            {
+                return (int)amount * 100;
+            }
+
+            private int PartGrivna(double amount)
+            {
+                return (int)amount;
+            }
+
+            private int PartCent(double amount)
+            {
+                return (int)amount;
+            }
+
+
+        }
+    }
+}
--- Money/Program.cs
+++ Money/Program.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IMenu = System.Collections.Generic.List<System.Tuple<string, GetMethod>>;
+using ItemMenu = System.Tuple<string, GetMethod>;
+using MenuSpace;
+public delegate void GetMethod();
 
-namespace Money
+namespace MoneySpace
 {
-    class Program
+
+
+
+
+    partial class Program
     {
+
         static void Main(string[] args)
         {
+            Money money = new Money();
+            Money money2 = new Money(15.13);
+
+            Console.WriteLine(money2);
+
+
         }
     }
 }
